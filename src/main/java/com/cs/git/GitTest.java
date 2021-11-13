ea6fa5480a6ab2c406b1fb0aee4566d677fe02c8
iwannaicecream <65487600+iwannaicecream@users.noreply.github.com>
2021-11-13 10:38:19 +0800

    pull test

--- src/main/java/com/cs/git/GitTest.java
+++ src/main/java/com/cs/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master fix");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
